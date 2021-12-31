e0f02fb593e9e59e6751460409cd96d2d186e3aa
skylow17 <echancerel@gmail.com>
2021-12-31 15:48:02 +0100

    Ajoutez des fichiers projet.

---
+++ Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿
+namespace GCodeSequencer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cmb_port = new System.Windows.Forms.ComboBox();
+            this.btn_connect = new System.Windows.Forms.Button();
+            this.btn_refresh = new System.Windows.Forms.Button();
+            this.cmb_baudrate = new System.Windows.Forms.ComboBox();
+            this.btn_openFile = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_sendSimple = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cmb_baudrate);
+            this.groupBox1.Controls.Add(this.btn_refresh);
+            this.groupBox1.Controls.Add(this.cmb_port);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(239, 88);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "COM";
+            // 
+            // cmb_port
+            // 
+            this.cmb_port.FormattingEnabled = true;
+            this.cmb_port.Location = new System.Drawing.Point(6, 28);
+            this.cmb_port.Name = "cmb_port";
+            this.cmb_port.Size = new System.Drawing.Size(146, 21);
+            this.cmb_port.TabIndex = 0;
+            // 
+            // btn_connect
+            // 
+            this.btn_connect.Location = new System.Drawing.Point(89, 106);
+            this.btn_connect.Name = "btn_connect";
+            this.btn_connect.Size = new System.Drawing.Size(75, 23);
+            this.btn_connect.TabIndex = 1;
+            this.btn_connect.Text = "Connect";
+            this.btn_connect.UseVisualStyleBackColor = true;
+            this.btn_connect.Click += new System.EventHandler(this.btn_connect_Click);
+            // 
+            // btn_refresh
+            // 
+            this.btn_refresh.Location = new System.Drawing.Point(158, 28);
+            this.btn_refresh.Name = "btn_refresh";
+            this.btn_refresh.Size = new System.Drawing.Size(75, 23);
+            this.btn_refresh.TabIndex = 2;
+            this.btn_refresh.Text = "Refresh";
+            this.btn_refresh.UseVisualStyleBackColor = true;
+            this.btn_refresh.Click += new System.EventHandler(this.btn_refresh_Click);
+            // 
+            // cmb_baudrate
+            // 
+            this.cmb_baudrate.FormattingEnabled = true;
+            this.cmb_baudrate.Items.AddRange(new object[] {
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "115200"});
+            this.cmb_baudrate.Location = new System.Drawing.Point(6, 55);
+            this.cmb_baudrate.Name = "cmb_baudrate";
+            this.cmb_baudrate.Size = new System.Drawing.Size(146, 21);
+            this.cmb_baudrate.TabIndex = 3;
+            this.cmb_baudrate.Text = "9600";
+            // 
+            // btn_openFile
+            // 
+            this.btn_openFile.Location = new System.Drawing.Point(89, 164);
+            this.btn_openFile.Name = "btn_openFile";
+            this.btn_openFile.Size = new System.Drawing.Size(75, 23);
+            this.btn_openFile.TabIndex = 2;
+            this.btn_openFile.Text = "Open File";
+            this.btn_openFile.UseVisualStyleBackColor = true;
+            this.btn_openFile.Click += new System.EventHandler(this.btn_openFile_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "\"Fichiers GCode|*.gcode|Tous les fichiers|*.*\"";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(283, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(460, 277);
+            this.listBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(159, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Baudrate";
+            // 
+            // btn_sendSimple
+            // 
+            this.btn_sendSimple.Location = new System.Drawing.Point(583, 295);
+            this.btn_sendSimple.Name = "btn_sendSimple";
+            this.btn_sendSimple.Size = new System.Drawing.Size(160, 23);
+            this.btn_sendSimple.TabIndex = 4;
+            this.btn_sendSimple.Text = "Send selected position";
+            this.btn_sendSimple.UseVisualStyleBackColor = true;
+            this.btn_sendSimple.Click += new System.EventHandler(this.btn_sendSimple_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(583, 324);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Start with timer";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(594, 354);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(97, 17);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "From beginning";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(594, 377);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(130, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.Text = "From selected position";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(677, 324);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(66, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(755, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btn_sendSimple);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btn_openFile);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btn_connect);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cmb_baudrate;
+        private System.Windows.Forms.Button btn_refresh;
+        private System.Windows.Forms.ComboBox cmb_port;
+        private System.Windows.Forms.Button btn_connect;
+        private System.Windows.Forms.Button btn_openFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_sendSimple;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Security;
+using System.IO;
+
+namespace GCodeSequencer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            updatePorts();
+        }
+        private SerialPort ComPort = new SerialPort();
+        private void updatePorts()
+        {
+
+            cmb_port.Items.Clear();
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                cmb_port.Items.Add(port);
+            }
+        }
+
+        private void btn_connect_Click(object sender, EventArgs e)
+        {
+            bool error = false;
+
+            if (!ComPort.IsOpen)
+            {
+                if ((cmb_port.SelectedIndex != -1) & (cmb_baudrate.SelectedIndex != -1))
+                {
+
+                    ComPort.PortName = cmb_port.Text;
+                    ComPort.BaudRate = int.Parse(cmb_baudrate.Text);
+
+                    try
+                    {
+                        ComPort.Open();
+                    }
+                    catch (UnauthorizedAccessException) { error = true; }
+                    catch (System.IO.IOException) { error = true; }
+                    catch (ArgumentException) { error = true; }
+
+                    if (error) MessageBox.Show("Impossible d'ouvrir le port");
+                }
+                else
+                {
+                    MessageBox.Show("Veuillez selectionner tous les paramètres");
+                }
+
+                if (ComPort.IsOpen)
+                {
+                    btn_connect.Text = "Disconnect";
+                    groupBox1.Enabled = false;
+                }
+            }
+            else
+            {
+                ComPort.Close();
+                btn_connect.Text = "Connect";
+                groupBox1.Enabled = true;
+            }
+
+        }
+
+        private void btn_refresh_Click(object sender, EventArgs e)
+        {
+            updatePorts();
+        }
+
+        private void btn_openFile_Click(object sender, EventArgs e)
+        {
+            string MyGCode;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var sr = new StreamReader(openFileDialog1.FileName);
+                    while((MyGCode = sr.ReadLine()) != null)
+                    {
+                        listBox1.Items.Add(MyGCode);
+                    }
+
+                }
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+                }
+
+            }
+        }
+
+        private void btn_sendSimple_Click(object sender, EventArgs e)
+        {
+            ComPort.Write(listBox1.SelectedItem.ToString() + '\n');
+        }
+    }
+}
